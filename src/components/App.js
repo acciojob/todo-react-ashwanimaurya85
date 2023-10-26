f7417eb2fa54c816a5bf96373398029efbba980c
Ashwani Kumar <130587125+ashwanimaurya85@users.noreply.github.com>
2023-10-26 11:26:08 +0000

    first

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,50 @@
 
-import React from "react";
+
 import './../styles/App.css';
 
+import React, { useState } from "react";
+
 const App = () => {
+  // State to manage the list of tasks
+  const [tasks, setTasks] = useState([]);
+  // State to manage the input field value
+  const [taskInput, setTaskInput] = useState("");
+
+  // Function to add a new task to the list
+  const addTask = () => {
+    if (taskInput.trim() !== "") {
+      setTasks([...tasks, taskInput]);
+      setTaskInput("");
+    }
+  };
+
+  // Function to remove a task from the list
+  const removeTask = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={taskInput}
+        onChange={(e) => setTaskInput(e.target.value)}
+        placeholder="Add a new task"
+      />
+      <button onClick={addTask}>Add Todo</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => removeTask(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
+
